3e143e778f1a58dd464d56442a17445121da9d4b
sue0711 <godqlswlq@gmail.com>
2015-05-22 02:53:40 +0900

    dd

--- Assets/Scripts/BallCotrol.cs
+++ Assets/Scripts/BallCotrol.cs
@@ -2,15 +2,17 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public enum BallState{
-	Wait,
-	Clicked,
-	Move
+public enum BallState
+{
+    Wait,
+    Clicked,
+    Move
 }
 
 
 
-public class BallCotrol : MonoBehaviour {
+public class BallCotrol : MonoBehaviour
+{
 
     public const int Color1 = 0;
     public const int Color3 = 1;
@@ -19,32 +21,32 @@ public class BallCotrol : MonoBehaviour {
     public const int Color8 = 4;
     public const int Color10 = 5;
     public const int Color13 = 6;
-    public const int Color14 = 7; 
-    
-	public int end_cnt;
-    
+    public const int Color14 = 7;
+
+    public int end_cnt;
+
     public BallState B_state;
 
-	GameObject arrow;
-	GameObject bg;
-    
+    GameObject arrow;
+    GameObject bg;
 
-	public GameObject[] figures;
-	public GameObject[] sameColor;
 
-	public float direction = 0f;
-	public float force = 40f;
-	
-	Transform myTransform;
-	Transform arrowTransform;
+    public GameObject[] figures;
+    public GameObject[] sameColor;
 
-	int sw;
-	public int remain_cnt;
-	public int bump_cnt;
+    public float direction = 0f;
+    public float force = 40f;
 
-	public Rigidbody2D rb;
-	SpriteRenderer sr;
-	SpriteRenderer bgSr;
+    Transform myTransform;
+    Transform arrowTransform;
+
+    int sw;
+    public int remain_cnt;
+    public int bump_cnt;
+
+    public Rigidbody2D rb;
+    SpriteRenderer sr;
+    SpriteRenderer bgSr;
     SpriteRenderer sr_ColorBar;
 
     public GameObject colorBarObj;
@@ -53,7 +55,7 @@ public class BallCotrol : MonoBehaviour {
 
     Vector3 velocity;
 
-	Text clearText;
+    Text clearText;
     Text endCnt;
 
     // Resource Array
@@ -72,27 +74,28 @@ public class BallCotrol : MonoBehaviour {
     public bool isCollided = false;
     public GameObject collidedObj;
 
-	void Start () {
+    void Start()
+    {
 
-		B_state = BallState.Wait;
-		myTransform = this.gameObject.transform;
-		rb = this.GetComponent<Rigidbody2D> ();
-		sr = this.GetComponent<SpriteRenderer> ();
+        B_state = BallState.Wait;
+        myTransform = this.gameObject.transform;
+        rb = this.GetComponent<Rigidbody2D>();
+        sr = this.GetComponent<SpriteRenderer>();
         sr_ColorBar = GameObject.Find("ColorChangeBar").GetComponent<SpriteRenderer>();
         colorBarObj = GameObject.Find("ColorChangeBar");
 
         rb_velocity = rb.velocity;
 
-		arrow = GameObject.Find ("ball/arrow");
-		arrowTransform = arrow.transform;
+        arrow = GameObject.Find("ball/arrow");
+        arrowTransform = arrow.transform;
 
-		bg = GameObject.Find ("BG");
-		bgSr = bg.GetComponent<SpriteRenderer> ();
-       
+        bg = GameObject.Find("BG");
+        bgSr = bg.GetComponent<SpriteRenderer>();
 
-		sw = 1;
-		bump_cnt = 0;
-		remain_cnt = 0;
+
+        sw = 1;
+        bump_cnt = 0;
+        remain_cnt = 0;
 
         clearText = GameObject.Find("Canvas/ResultText").GetComponent<Text>();
         endCnt = GameObject.Find("Canvas/EndCnt").GetComponent<Text>();
@@ -102,9 +105,9 @@ public class BallCotrol : MonoBehaviour {
 
         player1 = Resources.LoadAll<Sprite>("player_mal2/player1"); //8개
         player2 = Resources.LoadAll<Sprite>("player_mal2/player2");   //8개
-        background = Resources.LoadAll<Sprite>("background") ;    //8개
-        circle = Resources.LoadAll<Sprite>("Circle") ;    //8개
-        hexagon = Resources.LoadAll<Sprite>("hexagon") ;   //8개
+        background = Resources.LoadAll<Sprite>("background");    //8개
+        circle = Resources.LoadAll<Sprite>("Circle");    //8개
+        hexagon = Resources.LoadAll<Sprite>("hexagon");   //8개
         triangle = Resources.LoadAll<Sprite>("triangle");  //8개
         square = Resources.LoadAll<Sprite>("square");    //8개
         star = Resources.LoadAll<Sprite>("star");  //8개
@@ -113,9 +116,9 @@ public class BallCotrol : MonoBehaviour {
         ColorBar = Resources.LoadAll<Sprite>("color_change_bar"); //8개
 
         Debug.Log(player1[1]);
-	}
-	
-	// Update is called once per frame
+    }
+
+    // Update is called once per frame
     void Update()
     {
         if (B_state == BallState.Wait)
@@ -155,8 +158,6 @@ public class BallCotrol : MonoBehaviour {
                 velocity = myTransform.localRotation * Vector2.right;
                 rb.AddForce(velocity * force * 5.0f);
                 arrow.SetActive(false);
-
-                // B_state = BallState.Move;
             }
         }
 
@@ -165,9 +166,6 @@ public class BallCotrol : MonoBehaviour {
 
             rb.velocity = rb.velocity * 0.995f;
             Debug.Log("rb.velocity = " + rb.velocity);
-            //Debug.Log(rb.velocity);
-
-            //if(rb.velocity.x < 0.5f && rb.velocity.y < 0.5f)
             Invoke("GetVelocity", 1.0f);
         }
 
@@ -187,7 +185,7 @@ public class BallCotrol : MonoBehaviour {
             collidedObj.gameObject.transform.localScale = Vector3.Lerp(collidedObj.gameObject.transform.localScale,
                                                         new Vector3(7.0f, 7.0f, 1.0f), Time.deltaTime * 8.0f);
             Debug.Log("들어옴");
-           
+
             if (collidedObj.gameObject.transform.localScale.x >= 6.5f)
             {
                 isCollided = false;
@@ -195,21 +193,17 @@ public class BallCotrol : MonoBehaviour {
                 SameColorProcess();
             }
         }
-
-
     }
 
-
-
-
-	void GetVelocity(){
-		if((rb.velocity.x < 0.3f && rb.velocity.x > -0.3f)||(rb.velocity.y < 0.3f && rb.velocity.y > -0.3f))
+    void GetVelocity()
+    {
+        if ((rb.velocity.x < 0.3f && rb.velocity.x > -0.3f) || (rb.velocity.y < 0.3f && rb.velocity.y > -0.3f))
         {
-			rb.velocity = new Vector2(0.0f, 0.0f);
-			B_state = BallState.Wait;
+            rb.velocity = new Vector2(0.0f, 0.0f);
+            B_state = BallState.Wait;
             Debug.Log("GetVelocity() 실행");
-		}
-	}
+        }
+    }
 
     public void OnCollisionEnter2D(Collision2D other)
     {
@@ -245,7 +239,8 @@ public class BallCotrol : MonoBehaviour {
             sameColor = GameObject.FindGameObjectsWithTag(other.gameObject.tag);
 
             rb.velocity = new Vector2(0.0f, 0.0f);
-            myTransform.position = new Vector3(0.0f, 1.65f, 0.0f);
+            Vector3 temp = myTransform.position;
+            myTransform.position = temp;
             B_state = BallState.Wait;
 
         }
@@ -263,12 +258,23 @@ public class BallCotrol : MonoBehaviour {
                 {
                     if (figures[i].gameObject.name == sc.gameObject.name)
                     {
+
+                        for (int j = 0; j < figures.Length; j++)
+                        {
+                            if (figures[j].GetComponent<Renderer>().sortingLayerID == sc.GetComponent<Renderer>().sortingLayerID)
+                            {
+                                Debug.Log(j + "sc.gameObject.name = " + sc.gameObject.name);
+                                figures[j].GetComponent<Renderer>().sortingOrder--;
+                                Debug.Log("figures[i] = " + figures[j]);
+                            }
+                        }
+
                         figures[i] = new GameObject("Destory");
                         figures[i].AddComponent<SpriteRenderer>();
                         remain_cnt++;
                     }
                 }
-
+                /*
                 for (int i = 0; i < figures.Length; i++)
                 {
                     if (figures[i].GetComponent<Renderer>().sortingLayerID == sc.GetComponent<Renderer>().sortingLayerID)
@@ -277,7 +283,7 @@ public class BallCotrol : MonoBehaviour {
                         figures[i].GetComponent<Renderer>().sortingOrder--;
                         Debug.Log("figures[i] = " + figures[i]);
                     }
-                }
+                }*/
                 sc.gameObject.SetActive(false);
             }
         }
@@ -286,27 +292,28 @@ public class BallCotrol : MonoBehaviour {
     void ChangeBackground(string tag)
     {
         if (tag == "Color1") { bgSr.sprite = background[Color1]; bg.gameObject.tag = "Color1"; }
-        else if (tag == "Color3") { bgSr.sprite = background[Color3]; bg.gameObject.tag = "Color3"; } 
-        else if (tag == "Color5") { bgSr.sprite = background[Color5]; bg.gameObject.tag = "Color5"; } 
-        else if (tag == "Color7") { bgSr.sprite = background[Color7]; bg.gameObject.tag = "Color7"; } 
-        else if (tag == "Color8") { bgSr.sprite = background[Color8]; bg.gameObject.tag = "Color8"; } 
-        else if (tag == "Color10") {  bgSr.sprite = background[Color10]; bg.gameObject.tag = "Color10"; } 
+        else if (tag == "Color3") { bgSr.sprite = background[Color3]; bg.gameObject.tag = "Color3"; }
+        else if (tag == "Color5") { bgSr.sprite = background[Color5]; bg.gameObject.tag = "Color5"; }
+        else if (tag == "Color7") { bgSr.sprite = background[Color7]; bg.gameObject.tag = "Color7"; }
+        else if (tag == "Color8") { bgSr.sprite = background[Color8]; bg.gameObject.tag = "Color8"; }
+        else if (tag == "Color10") { bgSr.sprite = background[Color10]; bg.gameObject.tag = "Color10"; }
         else if (tag == "Color13") { bgSr.sprite = background[Color13]; bg.gameObject.tag = "Color13"; }
-        else if (tag == "Color14") { bgSr.sprite = background[Color14]; bg.gameObject.tag = "Color14"; } 
+        else if (tag == "Color14") { bgSr.sprite = background[Color14]; bg.gameObject.tag = "Color14"; }
     }
 
     // 공의 컬러와 컬러체인지바 같이 바꾸는 함수
-	void ColorChange(){
-		bump_cnt = bump_cnt % 8;
-		switch (bump_cnt)
+    void ColorChange()
+    {
+        bump_cnt = bump_cnt % 8;
+        switch (bump_cnt)
         {
-		    case 0:
+            case 0:
                 sr.sprite = player1[0];
-			    this.gameObject.tag = "Color1";
+                this.gameObject.tag = "Color1";
 
                 colorBarObj.gameObject.tag = "Color1";
                 sr_ColorBar.sprite = ColorBar[Color1];
-			    break;
+                break;
             case 1:
                 sr.sprite = player1[2];
                 this.gameObject.tag = "Color3";
@@ -356,9 +363,9 @@ public class BallCotrol : MonoBehaviour {
                 colorBarObj.gameObject.tag = "Color14";
                 sr_ColorBar.sprite = ColorBar[Color14];
                 break;
-			
-		}
-	}
-	
+
+        }
+    }
+
 }
 
